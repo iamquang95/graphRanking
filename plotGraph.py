fc13fbb3db2d14da1215c014acf6545a8464fdbf
iamquang95 <iamquang95@gmail.com>
2015-12-28 22:11:40 +0700

    Add pie chart score distribution

--- plotGraph.py
+++ plotGraph.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
 import csv
+import matplotlib.pyplot as plt
 
 
 class Record:
@@ -40,7 +44,42 @@ def read_data(problem_name, record_list):
                 record_list.append(Record(row[0], row[1], row[2:-1]))
             cnt += 1
 
+
+def show_line_chart_score():
+    score = []
+    for record in record_list:
+        score.append(sum(record.score[0:6]))
+    score = sorted(score)
+    print score
+
+    fig, ax = plt.subplots()
+    ax.set_title('Total 2 days')
+
+    xs = range(0, len(score))
+    ys = score
+    ax.plot(xs, ys)
+
+    plt.show()
+
+
+def show_bar_chart_score_distribution():
+    thresholds = [0, 10, 17, 23, 30, 40]
+    score = []
+    for record in record_list:
+        score.append(sum(record.score[0:6]))
+    labels = ['< 10', '10-17', '17-23', '23-30', '> 30']
+    explode = [0, 0, 0, 0, 0]
+    fracs = [0, 0, 0, 0, 0]
+    for x in score:
+        for i in xrange(0, len(fracs)):
+            if (thresholds[i] <= x) and (x <= thresholds[i + 1]):
+                fracs[i] += 1
+                break
+    colors = ['#756565', '#CCC2C2', '#965A38', '#A8A8A8', '#C98910']
+    plt.pie(fracs, explode=explode, labels=labels, colors=colors,
+            autopct='%1.1f%%', startangle=90)
+    plt.show()
+
 read_data(problem_name, record_list)
-# print problem_name
-# print (record_list[0].get_rank(), record_list[0].get_account(),
-#        record_list[0].get_score())
+# show_line_chart_score()
+show_bar_chart_score_distribution()
